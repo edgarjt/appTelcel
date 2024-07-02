a091e1ec12e1e1600876ba5dda91492e1904872a
Edgar Salomon <edgarjt97@gmail.com>
2024-07-01 20:02:44 -0600

    Implementación webapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,47 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demoTelcel';
+  deferredPrompt: any;
+  showInstallButton: boolean = false;
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onBeforeInstallPrompt(event: Event) {
+    event.preventDefault();
+    this.deferredPrompt = event;
+    this.showInstallButton = true;
+  }
+
+  constructor(private _snackBar: MatSnackBar) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      if (this.showInstallButton) {
+        this.openSnackBar('¿Deseas instalar la app?', 'Instalar');
+      }
+    }, 2500);
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action).onAction().subscribe(() => {
+      this.installApp();
+    });
+  }
+
+  installApp() {
+    this.showInstallButton = false;
+    if (this.deferredPrompt) {
+      this.deferredPrompt.prompt();
+      this.deferredPrompt.userChoice.then((choiceResult: any) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        this.deferredPrompt = null;
+      });
+    }
+  }
+
 }
