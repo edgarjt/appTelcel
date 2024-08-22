9d8ab2355d27b0d3d47ff6457f80cded342c39b2
Edgar Salomon <edgarjt97@gmail.com>
2024-08-21 20:03:51 -0600

    Se crea archivo de prueba

---
+++ .github/workflows/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Crear un directorio de prueba en la raíz
+mkdir -p /directorio_de_prueba
+
+# Crear un archivo de prueba dentro del directorio
+echo "Este es un archivo de prueba" > /directorio_de_prueba/archivo_prueba.txt
