13fcb705e50ebf7f3257cf2ae12593d4da74fe8b
Edgar Salomon <edgarjt97@gmail.com>
2024-08-21 21:41:21 -0600

    Se crea log de pruebas

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Crear un directorio de prueba en la raíz
-mkdir -p /directorio_de_prueba
-
-# Crear un archivo de prueba dentro del directorio
-echo "Este es un archivo de prueba" >sudo tee /directorio_de_prueba/archivo_prueba.txt > /dev/null
+# Crear un archivo de log en /var/log
+echo "$(date): Workflow ejecutado correctamente en el self-hosted runner." >> /var/log/mi_workflow.log
