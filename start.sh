135160df745d3468108bbc55943449ff8e74f16b
jimenezes_GHCO <jimenezes@globalhitss.com>
2024-08-22 10:27:57 -0600

    Se cre nuevo test

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Crear un archivo de log en /tmp
-echo "$(date): Workflow ejecutado correctamente en el self-hosted runner." >> /tmp/mi_workflow1.log
+echo "$(date): Workflow ejecutado correctamente en el self-hosted runner." >> /tmp/mi_workflow2.log
